1300e752636d48869534d6690cd06a9b2e0e4f17
Johnny <Johnny@BelladeMacBook-Air.local>
2023-03-17 13:20:56 +0800

    [Del] remove base config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   title: "程序员老曹的博客",
   description: "这是我的个人博客,记录我的生活及学习",
   srcDir: "src",
-  base: "/my_blog/",
+  // base: "/my_blog/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
